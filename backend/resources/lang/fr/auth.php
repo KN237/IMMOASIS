af6f9ccd2524fd0fda22548b45bef9276f9c61a2
Nagato-01 <nagorfonkoua@gmail.com>
2021-08-28 10:44:50 +0200

    test lang 2

--- backend/resources/lang/fr/auth.php
+++ backend/resources/lang/fr/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
-    'password' => 'Le mot de passe fourni est g incorrect.',
+    'password' => 'Le mot de passe fourni est incorrect.',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
 ];
